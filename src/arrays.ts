8efe7c774984cf5d7ca81d89b5b70f30608ee23f
maxmartapp <maxmartapp@gmail.com>
2022-09-27 10:23:05 +0300

    updated array

---
+++ src/arrays.ts
@@ -0,0 +1,16 @@
+const carMakers: string[] = ["ford", "toyoya", "nisan"];
+
+const Makers: string[] = [];
+
+const Bakers = [];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [];
+
+// help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+});
+
+// multiple types
+const importantDates = [new Date(), "2022-09-27", 25];
